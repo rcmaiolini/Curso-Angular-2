80cedcabadfdff1b2d532ec0055e70c92d990a7f
ricardo.maiolini <ricardo.maiolini@grupomalta.com.br>
2018-06-01 16:04:55 -0300

    Ate o video 16

--- primeiro-projeto/src/app/output-property/output-property.component.ts
+++ primeiro-projeto/src/app/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -12,20 +12,22 @@ export class OutputPropertyComponent implements OnInit {
 
   @Output() mudouValor = new EventEmitter();
 
+  @ViewChild('campoInput') campoValorInput: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
   }
 
   incrementa() {
-    this.valor++;
+    this.campoValorInput.nativeElement.value++;
     this.mudouValor.emit({
       novoValor: this.valor
     });
   }
 
   decrementa() {
-    this.valor--;
+    this.campoValorInput.nativeElement.value--;
     this.mudouValor.emit({
       novoValor: this.valor
     });
